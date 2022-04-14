71eab2c798ee762ed0bad628e4dfd3ed6a2f16ce
OmarAbouzeid95 <98395747+OmarAbouzeid95@users.noreply.github.com>
2022-04-14 23:58:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const closeBtn = document.getElementById("close-btn");
 
 function readTextFile(file){
     var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
+    rawFile.open("GET", file, true);
     rawFile.onreadystatechange = function(){
         if (rawFile.readyState === 4){
             if (rawFile.status === 200 || rawFile.status == 0) {
